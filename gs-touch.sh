dbef96583a61350f71e6cf70512d0855a9524162
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2019-03-13 13:40:15 +0100

    Fix variable referencing in gs-touch.sh

--- gs-touch.sh
+++ gs-touch.sh
@@ -4,8 +4,8 @@
 # Move file in GCS bucket in order to trigger events (e.g. for cloud functions)
 #
 
-$gs_file="${1?}"
-$tmp_file="$(dirname "$gs_file")/.tmp_$(basename "$gs_file")"
-echo "Moving '$tmp_file' to '$gs_file' and back..."
+gs_file="${1?}"
+tmp_file="$(dirname "$gs_file")/.tmp_$(basename "$gs_file")"
+echo "Moving '$gs_file' to '$tmp_file' and back..."
 gsutil mv "$gs_file" "$tmp_file" && gsutil mv "$tmp_file" "$gs_file"
 
