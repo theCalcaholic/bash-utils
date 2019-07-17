3faf44c85ecc9795eda68fb97dc78d2fc8940a9d
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2019-07-18 00:29:41 +0200

    Add command paramter to keepass-mounter.sh
    Add --batch mode to start-when-available.sh

--- keepass-mounter.sh
+++ keepass-mounter.sh
@@ -5,7 +5,7 @@
 # a vault in said directory
 #
 # Requires 
-#   - the flatpak version of KeepassXC to be installed
+#   - the flatpak version of KeepassXC to be installed (or whatever version is passed via the -c argument)
 #   - an fstab entry for the desired mount point
 #
 
@@ -17,6 +17,7 @@ print_usage() {
     echo "      -b, --backup An (optional) path to store a backup of the database in"
     echo "      -m, --mount The directory to mount the remote storage into"
     echo "      -f, --file  The path to your keepass vault (relative to the mount point)"
+    echo "      -c, --command The command for executing keepass. '--DB_PATH--' will be replaced with the path to the password database."
     echo ""
     echo "Example:"
     echo "  keepass-mounter -m /media/myUser/keepass -f myvault.kdbx -b ~/keepass-backups"
@@ -26,6 +27,7 @@ trap print_usage 1 2
 
 mount_path="/media/keepass"
 vault_path="vault.kdbx"
+keepass_cmd_pattern='flatpak run --file-forwarding org.keepassxc.KeePassXC @@ "--DB_PATH--" @@'
 
 expected=""
 backup_dir=""
@@ -49,6 +51,9 @@ do
     elif [ "$expected" == "file" ]
     then
         vault_path="${arg%/}"
+    elif [ "$expected" == "command" ]
+    then
+        keepass_cmd_pattern="$arg"
     fi
 
     if [ ! -z "$expected" ]
@@ -67,8 +72,12 @@ do
     then
         print_usage
         exit 0
-    else [ "$arg" == "--file" ] || [ "$arg" == "-f" ]
+    elif [ "$arg" == "--file" ] || [ "$arg" == "-f" ]
+    then
         expected="file"
+    elif [ "$arg" == "--command" ] || [ "$arg" == "-c" ]
+    then
+        expected="command"
     fi
 done
 
@@ -90,4 +99,9 @@ then
 
     cp "$mount_path/$vault_path" "$backup_dir/$backup_file"
 fi
-nohup flatpak run --file-forwarding org.keepassxc.KeePassXC @@ "${mount_path%/}/$vault_path" @@ &
+
+#nohup flatpak run --file-forwarding org.keepassxc.KeePassXC @@ "${mount_path%/}/$vault_path" @@ &
+keepass_cmd="$(sed "s|--DB\_PATH--|${mount_path%/}/$vault_path|g" <<< "$keepass_cmd_pattern")"
+
+nohup bash -c "$keepass_cmd" &
+
