c85fc37c9a4b38c9063c8f0a6f018cf15e639cc8
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-03-06 19:53:30 +0100

    lower-vpn-priority.sh: Add --priority parameter

--- lower-vpn-priority.sh
+++ lower-vpn-priority.sh
@@ -5,13 +5,18 @@
 set -e
 shopt -s extglob
 
-USAGE="lower-vpn-priority.sh
+USAGE="lower-vpn-priority.sh [OPTIONS]
+
+  Options:
+    -p, --priority <value> Sets the new route priority to the given value (default: 101)
 
   Must be executed as root"
 DESCRIPTION="Lowers your VPNs default route priority to 101"
 
 . "$(dirname "$BASH_SOURCE")/lib/parse_args.sh"
+KEYWORDS=("-p" "--priority")
 parse_args __USAGE "$USAGE" __DESCRIPTION "$DESCRIPTION" "$@"
+priority="${KW_ARGS['--priority']-${KW_ARGS['-p']}}"
 
 if [[ "$EUID" -ne 0 ]]
 then
@@ -27,7 +32,7 @@ then
 fi
 
 ip route del $vpn_route
-ip route add ${vpn_route/metric +([0-9])/metric 101} || {
+ip route add ${vpn_route/metric +([0-9])/metric ${priority:-101}} || {
   echo "ERROR: Could not recreate vpn route! Your network device 'tun0' (hopefully your vpn) might not work correctly. If you encounter issues, try reconnecting.
 Alternatively, execute the command 'sudo ip route add ${vpn_route/metric +([0-9])/metric 101}' yourself."
   exit 3
